4787d72c562132cf0ae6230f50ebefbdd2e875e7
Alexander Gushchin <gav8704@mail.ru>
2020-10-25 19:56:34 +0700

    creating SearchInput component

---
+++ src/components/SearchInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../styles/components/search-input.scss'
+
+const SearchInput = () => {
+
+  return (
+    <>
+      <input
+        className="search-input"
+        placeholder="Search for user"
+      />
+    </>
+  )
+}
+
+export default SearchInput
