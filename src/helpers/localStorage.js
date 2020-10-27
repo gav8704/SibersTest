582a07570419222fdb6e7f24caff4ba30fa6799d
Alexander Gushchin <gav8704@mail.ru>
2020-10-27 17:08:19 +0700

    adding localStorage helpers

---
+++ src/helpers/localStorage.js
@@ -0,0 +1,49 @@
+
+/**
+ * Check if key exists in local storage
+ * @param  string key
+ * @return boolean
+ */
+export const localStorageHas = key => {
+  const item = localStorage.getItem(key)
+
+  return item !== null 
+}
+
+/**
+ * Retrive an object from local storage.
+ * @param  string key
+ * @return mixed
+ */
+export const localStorageGet = key => {
+  const item = localStorage.getItem(key)
+
+  if (item[0] === '{' || item[0] === '[') {
+    return JSON.parse(item)
+  } else if (!item) {
+    return
+  } else {
+    return item
+  }
+}
+
+/**
+ * Save some value to local storage.
+ * @param string key    
+ * @param string value
+ */
+export const localStorageSave = (key, value) => {
+  if ( typeof(value) === 'object' ) {
+    value = JSON.stringify(value)
+  }
+
+  localStorage.setItem(key, value)
+}
+
+/**
+ * Remove element from local storage.
+ * @param string key 
+ */
+export const localStorageRemove = key => {
+  localStorage.removeItem(key)
+}
