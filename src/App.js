865c5e6b264615512fc7bbc26f69547e1c848b47
Alexander Gushchin <gav8704@mail.ru>
2020-10-27 17:10:21 +0700

    fetching userList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useEffect } from 'react'
 import { 
   Switch,
   Route, 
 } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { getUserList } from './api'
+import { setUserList } from './redux/actions'
+import { 
+  localStorageHas,
+  localStorageGet,
+  localStorageSave
+} from './helpers/localStorage'
 
 import Home from './pages/Home'
 
 import './styles/index.scss'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const localStorageHasUserList = localStorageHas('userList')
+
+    if (localStorageHasUserList) {
+      const userList = localStorageGet('userList')
+      setUserList(userList)
+    } else {
+      fetchUserList()
+    }
+  }, [])
+
+  function fetchUserList () {
+    getUserList()
+      .then(res => {
+        const userList = res.data
+
+        dispatch(setUserList(userList))
+        localStorageSave('userList', userList)
+      })
+  }
 
   return (
     <div className="app">
